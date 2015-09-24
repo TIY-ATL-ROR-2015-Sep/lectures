eaf6802613ca6f38ac1e99ce96d71f586a56e305
Brit Butler <redline6561@gmail.com>
2015-09-24 11:57:21 -0400

    Come up with a better algorithm to only compare words of same size.

--- wk01-ruby-basics/thu/cosmicomics.rb
+++ wk01-ruby-basics/thu/cosmicomics.rb
@@ -17,17 +17,25 @@ def join_words(word1, word2)
   word1 + word2[1..-1]
 end
 
+def build_dictionary(file)
+  dict = Hash.new([])
+  file.each_line do |line|
+    word = line.chomp
+    if word.length > 3
+      dict[word.length] += [word]
+    end
+  end
+  dict
+end
+
 def find_funny_words(filepath)
   File.open(filepath, "r") do |f|
-    dict = f.readlines.map { |word| word.chomp }
-    dict.combination(2).each do |word1, word2|
-      if funny_word?(word1, word2)
-        puts join_words(word1, word2)
+    word_groups = build_dictionary(f)
+    word_groups.each do |word_length, words|
+      words.combination(2).each do |word1, word2|
+       puts join_words(word1, word2) if funny_word?(word1, word2)
       end
     end
- # TODO: Room for performance improvement!
- #       if word1.length >= 4 && word2.length >= 4 && word1.length == word2.length
- #       end
   end
 end
 
