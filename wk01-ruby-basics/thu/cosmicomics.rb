a7a142cf793572d726e6d3a02bd68213eea777dd
Brit Butler <redline6561@gmail.com>
2015-09-24 11:43:01 -0400

    We find funny words now.

--- wk01-ruby-basics/thu/cosmicomics.rb
+++ wk01-ruby-basics/thu/cosmicomics.rb
@@ -17,6 +17,19 @@ def join_words(word1, word2)
   word1 + word2[1..-1]
 end
 
-binding.pry
+def find_funny_words(filepath)
+  File.open(filepath, "r") do |f|
+    dict = f.readlines.map { |word| word.chomp }
+    dict.combination(2).each do |word1, word2|
+      if funny_word?(word1, word2)
+        puts join_words(word1, word2)
+      end
+    end
+ # TODO: Room for performance improvement!
+ #       if word1.length >= 4 && word2.length >= 4 && word1.length == word2.length
+ #       end
+  end
+end
 
-puts "hell no."
+find_funny_words("english-dict.txt")
+
