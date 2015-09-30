97f1243d800a795b1d9c3147e74e46d4f37af7c8
Brit Butler <redline6561@gmail.com>
2015-09-30 11:26:11 -0400

    Add setup method for HumanTests.

--- wk02-object-oriented-design/wed/tests.rb
+++ wk02-object-oriented-design/wed/tests.rb
@@ -4,21 +4,25 @@ require './random'
 require 'minitest/autorun'
 
 class HumanPlayerTests < MiniTest::Test
+  def setup
+    @player = HumanPlayer.new
+    @brit = HumanPlayer.new("Brit")
+  end
+
   def test_can_make_player
-    assert HumanPlayer.new
-    assert HumanPlayer.new("Brit")
+    assert @player
+    assert @brit
   end
 
-  def test_can_get_name
-    brit = HumanPlayer.new("Brit")
-    assert_equal brit.name, "Brit"
+  def test_can_get_names
+    assert_equal @brit.name, "Brit"
+    assert_equal @player.name, "The Nameless One"
   end
 
-  def test_can_get_nameless
+  def test_can_get_guess
+    skip
     player = HumanPlayer.new
-    assert_equal player.name, "The Nameless One"
   end
-
 end
 
 class RandomPlayerTests < MiniTest::Test
