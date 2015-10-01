95e01e4dc91b23ce2957128fae214d1c3fb2399b
Brit Butler <redline6561@gmail.com>
2015-10-01 09:47:20 -0400

    Make first test pass. Hooray for Humans.

--- wk02-object-oriented-design/wed/hw/human.rb
+++ wk02-object-oriented-design/wed/hw/human.rb
@@ -1,2 +1,18 @@
 class Human
+  def initialize(name)
+    @name = name
+  end
+
+  def alertness
+    0.0
+  end
 end
+
+
+# Foo.new(bar, baz)
+## Ruby does this:
+
+### looks up the class Foo
+### allocates memory for an empty Foo, calls it x
+### x.initialize(bar, baz)  ... or whatever arguments new got
+### hands x back to us
